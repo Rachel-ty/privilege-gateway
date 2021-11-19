18efc97120c51bed3ea1b2284879302b02d8b629
mawangdan <mawangdan@foxmail.com>
2021-11-20 01:38:18 +0800

    2-10 author

--- privilegeservice/src/main/java/cn/edu/xmu/privilegegateway/privilegeservice/util/bloom/RedisBloomService.java
+++ privilegeservice/src/main/java/cn/edu/xmu/privilegegateway/privilegeservice/util/bloom/RedisBloomService.java
@@ -1,7 +1,9 @@
 package cn.edu.xmu.privilegegateway.privilegeservice.util.bloom;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.scripting.support.ResourceScriptSource;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,21 +16,13 @@ import java.util.List;
 public class RedisBloomService<T> {
     private RedisTemplate redisTemplate;
 
-    private String bloomFilterExistLua="local bloomName = KEYS[1]\n" +
-            "local value = KEYS[2]\n" +
-            "local result_1 = redis.call('BF.EXISTS', bloomName, value)\n" +
-            "return result_1";
-    private String bloomFilterAddLua="local bloomName = KEYS[1]\n" +
-            "local value = KEYS[2]\n" +
-            "local result_1 = redis.call('BF.ADD', bloomName, value)\n" +
-            "return result_1";
     public RedisBloomService(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
     }
 
     public Boolean bloomFilterAdd(String bloomFilterName,T value){
         DefaultRedisScript<Boolean> bloomAdd = new DefaultRedisScript<>();
-        bloomAdd.setScriptText(bloomFilterAddLua);
+        bloomAdd.setScriptSource(new ResourceScriptSource(new ClassPathResource("bloomFilterAdd.lua")));
         bloomAdd.setResultType(Boolean.class);
         List<Object> keyList= new ArrayList<>();
         keyList.add(bloomFilterName);
@@ -37,11 +31,9 @@ public class RedisBloomService<T> {
         return result;
     }
 
-
-
     public Boolean bloomFilterExists(String bloomFilterName,T value) {
         DefaultRedisScript<Boolean> bloomExists = new DefaultRedisScript<>();
-        bloomExists.setScriptText(bloomFilterExistLua);
+        bloomExists.setScriptSource(new ResourceScriptSource(new ClassPathResource("bloomFilterExist.lua")));
         bloomExists.setResultType(Boolean.class);
         List<Object> keyList = new ArrayList<>();
         keyList.add(bloomFilterName);
