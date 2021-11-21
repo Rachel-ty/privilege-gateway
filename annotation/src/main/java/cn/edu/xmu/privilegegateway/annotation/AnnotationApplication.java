99eaa0b413b4b2c17b5b5d8d66ec3d4bcbba2d4c
jxy <858297426@qq.com>
2021-11-21 09:03:16 +0800

    2-17修改注解

--- annotation/src/main/java/cn/edu/xmu/privilegegateway/annotation/AnnotationApplication.java
+++ annotation/src/main/java/cn/edu/xmu/privilegegateway/annotation/AnnotationApplication.java
@@ -8,8 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  **/
 @SpringBootApplication
 public class AnnotationApplication {
+
     public static void main(String[] args) {
+
         SpringApplication.run(AnnotationApplication.class, args);
+
     }
 
 }
