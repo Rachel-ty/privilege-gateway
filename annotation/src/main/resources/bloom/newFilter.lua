00a22a549f24504d106e3602f89aba4c34113779
Hasee <c170325225@163.com>
2021-11-23 13:13:28 +0800

    1.修改newFilter的lua脚本并加上注释；2.修改测试并加上注释

--- annotation/src/main/resources/bloom/newFilter.lua
+++ annotation/src/main/resources/bloom/newFilter.lua
@@ -1,10 +1,17 @@
+--作用：新建指定参数的filter
+
 local filterName = KEYS[1]
 local errorRate = KEYS[2]
 local capacity = KEYS[3]
 
-local result = redis.call("BF.RESERVE", filterName, errorRate, capacity)
-if result == "OK" then
-    return "1"
-else
-    return "0"
+--返回值：只有1项的表
+--["ok":"OK"] 新建成功
+--["err":"{err_msg}"] 新建失败
+local result = redis.pcall("BF.RESERVE", filterName, errorRate, capacity) --采用call会抛异常，采用pcall会返回错误信息
+for key, value in pairs(result) do
+    if key == 'ok' then
+        return true
+    else
+        return false
+    end
 end
--- annotation/src/test/java/cn/edu/xmu/privilegegateway/util/bloom/BloomFilterTest.java
+++ annotation/src/test/java/cn/edu/xmu/privilegegateway/util/bloom/BloomFilterTest.java
@@ -12,63 +12,105 @@ import static org.junit.jupiter.api.Assertions.*;
  * @date 2021-11-20
  * @sn 22920192204170
  */
+
 @SpringBootTest(classes = {AnnotationApplication.class})
 public class BloomFilterTest {
     @Autowired
-    private BloomFilter<String> filter;
+    private BloomFilter<String> stringFilter;
 
     @Autowired
-    private BloomFilter<Long> filterInt;
+    private BloomFilter<Long> longFilter;
 
+    /**
+     * 测试对filter的操作
+     */
     @Test
     public void filterTest() {
         String filterFor = "test";
 
-        filter.deleteFilter(filterFor);
+        stringFilter.deleteFilter(filterFor);
 
-        assertFalse(filter.newFilter(filterFor, -0.01, 100));
-        assertFalse(filter.newFilter(filterFor, 1.01, 100));
-        assertFalse(filter.newFilter(filterFor, 0.001, -1));
+        //测试参数不正确时的状况
+        assertFalse(stringFilter.newFilter(filterFor, -0.01, 100));
+        assertFalse(stringFilter.newFilter(filterFor, 1.01, 100));
+        assertFalse(stringFilter.newFilter(filterFor, 0.001, -1));
 
-        assertTrue(filter.newFilter(filterFor, 0.001, 100));
-        assertTrue(filter.checkFilter(filterFor));
-        assertTrue(filter.deleteFilter(filterFor));
-        assertFalse(filter.checkFilter(filterFor));
-        assertFalse(filter.deleteFilter(filterFor));
+        //测试新建filter成功的状况
+        assertTrue(stringFilter.newFilter(filterFor, 0.001, 100));
 
-        assertTrue(filter.newFilter(filterFor, null, null));
-        filter.deleteFilter(filterFor);
+        //测试重复新建filter的状况
+        assertFalse(stringFilter.newFilter(filterFor, 0.001, 100));
+
+        //测试新建的filter是否存在
+        assertTrue(stringFilter.checkFilter(filterFor));
+
+        //测试是否可以删除新建的filter
+        assertTrue(stringFilter.deleteFilter(filterFor));
+
+        //测试删除的filter是否存在
+        assertFalse(stringFilter.checkFilter(filterFor));
+
+        //测试重复删除filter的情况
+        assertFalse(stringFilter.deleteFilter(filterFor));
+
+        //测试以默认参数新建filter的情况
+        assertTrue(stringFilter.newFilter(filterFor, null, null));
+
+        stringFilter.deleteFilter(filterFor);
     }
 
+    /**
+     * 测试对StringFilter中的值操作
+     */
     @Test
-    public void valueTest() {
+    public void stringValueTest() {
         String filterFor = "test";
 
-        if(!filter.checkFilter(filterFor)) {
-            filter.newFilter(filterFor, 0.001, 100);
+        if(stringFilter.checkFilter(filterFor)) {
+            stringFilter.deleteFilter(filterFor);
         }
 
-        assertFalse(filter.checkValue(filterFor, "1"));
-        assertTrue(filter.addValue(filterFor, "1"));
-        assertTrue(filter.checkValue(filterFor, "1"));
-        assertFalse(filter.addValue(filterFor, "1"));
+        //检测不存在的filter中是否存在值
+        assertFalse(stringFilter.checkValue(filterFor, "1"));
 
-        filter.deleteFilter(filterFor);
+        stringFilter.newFilter(filterFor, 0.001, 100);
+
+        //检测新建的filter中是否存在值
+        assertFalse(stringFilter.checkValue(filterFor, "1"));
+
+        //检测是否可以往新建的filter中插入值
+        assertTrue(stringFilter.addValue(filterFor, "1"));
+
+        //检测插入的值是否存在
+        assertTrue(stringFilter.checkValue(filterFor, "1"));
+
+        //检测是否可以往filter中重复插入值
+        assertFalse(stringFilter.addValue(filterFor, "1"));
+
+        stringFilter.deleteFilter(filterFor);
     }
 
+    /**
+     * 测试对LongFilter中的值操作，与stringValueTest基本相同
+     */
     @Test
-    public void valueTestInteger() {
-        String filterFor = "testI";
+    public void longValueTest() {
+        String filterFor = "test";
 
-        if(!filterInt.checkFilter(filterFor)) {
-            filterInt.newFilter(filterFor, 0.001, 100);
+        if(longFilter.checkFilter(filterFor)) {
+            longFilter.deleteFilter(filterFor);
         }
 
-        assertFalse(filterInt.checkValue(filterFor, 1L));
-        assertTrue(filterInt.addValue(filterFor, 1L));
-        assertTrue(filterInt.checkValue(filterFor, 1L));
-        assertFalse(filterInt.addValue(filterFor, 1L));
+        //检测不存在的filter中是否存在值
+        assertFalse(longFilter.checkValue(filterFor, 1L));
 
-        filterInt.deleteFilter(filterFor);
+        longFilter.newFilter(filterFor, 0.001, 100);
+
+        assertFalse(longFilter.checkValue(filterFor, 1L));
+        assertTrue(longFilter.addValue(filterFor, 1L));
+        assertTrue(longFilter.checkValue(filterFor, 1L));
+        assertFalse(longFilter.addValue(filterFor, 1L));
+
+        longFilter.deleteFilter(filterFor);
     }
 }
