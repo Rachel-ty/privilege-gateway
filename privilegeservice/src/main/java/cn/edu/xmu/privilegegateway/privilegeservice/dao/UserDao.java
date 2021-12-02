c589f74121b693a05d643d7ac8fd7d05a0bae102
Ming Qiu <mingqiu@xmu.edu.cn>
2021-12-02 20:05:34 +0800

    修改key

--- privilegeservice/src/main/java/cn/edu/xmu/privilegegateway/privilegeservice/dao/UserDao.java
+++ privilegeservice/src/main/java/cn/edu/xmu/privilegegateway/privilegeservice/dao/UserDao.java
@@ -45,10 +45,17 @@ public class UserDao{
     @Value("${privilegeservice.user.expiretime}")
     private long timeout;
 
-    public final static String SINGLEUSERKEY="u_%d";
-    public final static String PROXYUSERKEY="f_%d";
-    public final static String USERKEY="up_%d";
+    public final static String FUSERKEY="f_%d";
+    /**
+     * 用户的redis key： u_id, 集合里为base role
+     *
+     */
+    private final static String USERKEY = "u_%d";
 
+    /**
+     * 最终用户的redis key: up_id 集合里为
+     */
+    private final static String USERPROXYKEY = "up_%d";
 
 
     @Autowired
@@ -94,16 +101,7 @@ public class UserDao{
 //    @Autowired
 //    private JavaMailSender mailSender;
 
-    /**
-     * 用户的redis key： u_id
-     *
-     */
-    private final static String USERKEY = "u_%d";
 
-    /**
-     * 最终用户的redis key: up_id
-     */
-    private final static String USERPROXYKEY = "up_%d";
 
     /**
      * @author yue hao
