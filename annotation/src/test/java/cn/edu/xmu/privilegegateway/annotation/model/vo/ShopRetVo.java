1ce011fe75e01153a2408c8f15d25a5c3352a6bf
Ming Qiu <mingqiu@xmu.edu.cn>
2021-12-04 08:20:12 +0800

    将一些方法移到annotion的common中

---
+++ annotation/src/test/java/cn/edu/xmu/privilegegateway/annotation/model/vo/ShopRetVo.java
@@ -0,0 +1,12 @@
+package cn.edu.xmu.privilegegateway.annotation.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class ShopRetVo {
+    private Byte state;
+}
